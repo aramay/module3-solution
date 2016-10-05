5b9c701fee12bbf6b81282acc65949283258e880
“kasper341” <kasper_341@hotmail.com>
2016-10-05 10:10:25 -0700

    setup controller to talk to view setup service to get menu items from remote url

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
 (function () {
 
-    var app = angular.module('Module3Solution', []);
+    var app = angular.module('NarrowItDownApp', []);
 
-    app.controller('');
+    app.controller('NarrowItDownController', NarrowItDownController);
+
+    app.service('MenuSearchService', MenuSearchService);
+    app.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController() {
+        console.log(MenuSearchService);
+
+        var ctrl = this;
+
+        ctrl.searchTerm = "";
+
+        // var temp = MenuSearchService.testFunc();
+
+        // temp.then = function (response) {
+        //
+        // };
+
+        ctrl.logMatchedMenuItems = function (searchTerm) {
+
+            console.log(searchTerm);
+
+        };
+
+        console.log(ctrl);
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+
+        var service = this;
+
+        service.testFunc = function () {
+            return "nill";
+        };
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            var response = {
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json"),
+                params: {
+                    category: searchTerm
+                }
+            };
+            return response;
+        };
+    }
+
 }) ();
